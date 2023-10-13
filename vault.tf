8ac3ed8bde9051b8c2b0f596a9f83b29dda3eb4b
Ricardo Oliveira <ricardo@hashicorp.com>
2023-10-13 12:24:09 +0100

    better naming . no root rotation

--- vault.tf
+++ vault.tf
@@ -3,19 +3,19 @@ provider "vault" {
   namespace = "admin"
 }
 
-resource "vault_namespace" "instruqt" {
+resource "vault_namespace" "demo" {
   path = "demo_${random_id.default.hex}"
 }
 
-resource "vault_mount" "default" {
-  namespace = vault_namespace.instruqt.path_fq
+resource "vault_mount" "database_mssql" {
+  namespace = vault_namespace.demo.path_fq
   path      = format("%s/database/mssql", google_sql_database_instance.default.name)
   type      = "database"
 }
 
-resource "vault_database_secret_backend_connection" "default" {
-  namespace     = vault_namespace.instruqt.path_fq
-  backend       = vault_mount.default.path
+resource "vault_database_secret_backend_connection" "cloudsql" {
+  namespace     = vault_namespace.demo.path_fq
+  backend       = vault_mount.database_mssql.path
   name          = google_sql_database_instance.default.name
   allowed_roles = ["dev", google_sql_user.default.name]
   plugin_name   = "mssql-database-plugin"
@@ -30,10 +30,10 @@ resource "vault_database_secret_backend_connection" "default" {
 }
 
 resource "vault_database_secret_backend_role" "default" {
-  namespace = vault_namespace.instruqt.path_fq
-  backend   = vault_mount.default.path
+  namespace = vault_namespace.demo.path_fq
+  backend   = vault_mount.database_mssql.path
   name      = "dev"
-  db_name   = vault_database_secret_backend_connection.default.name
+  db_name   = vault_database_secret_backend_connection.cloudsql.name
   creation_statements = [
     "USE [${google_sql_database.default.name}];",
     "CREATE LOGIN [{{name}}] WITH PASSWORD = '{{password}}';",
@@ -43,15 +43,25 @@ resource "vault_database_secret_backend_role" "default" {
   revocation_statements = [
     "USE [${google_sql_database.default.name}];",
     "DROP LOGIN [{{name}}];"
-    ]
+  ]
 }
+
+# resource "vault_generic_endpoint" "rotate_initial_db_password" {
+#   depends_on     = [vault_database_secret_backend_connection.cloudsql]
+#   namespace      = vault_namespace.demo.path_fq
+#   path           = "${vault_mount.database_mssql.path}/rotate-root/${vault_database_secret_backend_connection.cloudsql.name}"
+#   disable_read   = true
+#   disable_delete = true
+
+#   data_json = "{}"
+# }
 
 # configure a static role with period-based rotations
 # resource "vault_database_secret_backend_static_role" "static_role" {
-#   namespace = vault_namespace.instruqt.path_fq
-#   backend   = vault_mount.default.path
+#   namespace = vault_namespace.demo.path_fq
+#   backend   = vault_mount.database_mssql.path
 #   name                = google_sql_user.default.name
-#   db_name   = vault_database_secret_backend_connection.default.name
+#   db_name   = vault_database_secret_backend_connection.cloudsql.name
 #   username            = google_sql_user.default.name
 #   rotation_period     = "3600"
 #   rotation_statements = ["ALTER USER [{{name}}] WITH PASSWORD = '{{password}}';"]
