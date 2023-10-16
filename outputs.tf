b62d68be1dd25a1589e3608be2d9f8405203c642
Ricardo Oliveira <ricardo@hashicorp.com>
2023-10-16 11:52:49 +0100

    fixing the outputs

--- outputs.tf
+++ outputs.tf
@@ -9,7 +9,7 @@ output "vault_mssql_role" {
   value = vault_database_secret_backend_role.default.id
 }
 output "vault_command" {
-  value = "vault read -namespace=admin/${vault_namespace.demo.path_fq} ${vault_mount.database_mssql.path}/creds/${vault_database_secret_backend_connection.cloudsql.name}"
+  value = "vault read -namespace=admin/${vault_namespace.demo.path_fq} ${vault_mount.database_mssql.path}/creds/${vault_database_secret_backend_role.default.name}"
 }
 # output "vault_mssql_static_role" {
 #   value = vault_database_secret_backend_static_role.static_role.id
